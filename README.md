27bcab89b1d8516b07460f1d4abb715ee2a41824
Cyber MacGeddon <cybermaggedon@gmail.com>
2018-08-30 09:28:53 +0100

    Gaffer Python API

---
+++ README.md
@@ -0,0 +1,31 @@
+# PyGaffer
+
+API for Gaffer graph store.
+
+# Install
+`pip install git+git://github.com/cybermaggedon/pygaffer`
+
+# Example
+
+```
+import gaffer
+
+g = gaffer.Gaffer("http://localhost:8080")
+
+# For TLS, use keys in ~/private
+# g.use_cert()
+
+op1 = g.get_all(entities=["ip"], edges=["ipflow"])
+op2 = g.limit(30)
+ops = g.operation_chain([op1, op2])
+
+res = g.execute(ops)
+
+for v in res:
+    if v.has_key("source"):
+        print "Edge:", v["source"], "->", v["destination"]
+
+    if v.has_key("vertex"):
+        print "Node:", v["vertex"]
+
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyGaffer",
+    version="0.1",
+    author="cybermaggedon",
+    author_email="cybermaggedon@gmail.com",
+    description="Python Gaffer API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cybermaggedon/pygaffer",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
